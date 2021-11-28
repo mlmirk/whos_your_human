4c99b2191b291d145899bbc8e1ebbebea0e304ed
mlmirk <mike.l.mirkovic@gmail.com>
2021-11-28 13:40:23 -0500

    Full Crud and .env created

--- whos_your_human/settings.py
+++ whos_your_human/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 from pathlib import Path
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+x77f72dxze1$@cryj8w@y%f=duf785fdk!90fm*#%knel-7c('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
